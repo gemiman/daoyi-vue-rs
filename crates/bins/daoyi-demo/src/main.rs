ad9a9b8685603758c3ca9f47eaf220160309a3ea
Gemiman <913573586@qq.com>
2025-12-20 01:54:36 +0800

    feat(axum): 添加宏特性并优化服务器路由处理
    - 在axum依赖中启用macros特性以支持宏功能
    - 将路由处理函数由闭包改为带有debug_handler属性的独立异步函数
    - 优化println语句格式，提升可读性
    - 绑定TcpListener监听本地0.0.0.0:3000地址并启动服务

--- crates/bins/daoyi-demo/src/main.rs
+++ crates/bins/daoyi-demo/src/main.rs
@@ -1,13 +1,21 @@
-use axum::{Router, routing};
+use axum::{Router, debug_handler, routing};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", routing::get(async || "Hello, Daoyi Vue Rust!"));
+    let router = Router::new().route("/", routing::get(index));
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
-    println!("Server is listening on: http://{}", listener.local_addr().unwrap());
+    println!(
+        "Server is listening on: http://{}",
+        listener.local_addr().unwrap()
+    );
 
     axum::serve(listener, router).await.unwrap();
+}
+
+#[debug_handler]
+async fn index() -> &'static str {
+    "Hello, Daoyi Vue Rust!"
 }
