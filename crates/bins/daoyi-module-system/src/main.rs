85d03e8ef26a96165a92b36c235d1913c6738c7e
Gemiman <913573586@qq.com>
2025-12-25 12:40:27 +0800

    feat(build): 新增实体与API系统模块及主程序异步启动
    - 新增 daoyi-entity-system 和 daoyi-api-system 两个库
    - 在主仓库 Cargo.toml 中添加新模块依赖
    - 配置 daoyi-api-system 依赖及空路由创建函数
    - daoyi-entity-system 实现简单测试的加法函数
    - 修改 daoyi-module-system 主程序为异步 tokio 入口
    - 添加相关依赖到 daoyi-module-system 的 Cargo.toml 文件

--- crates/bins/daoyi-module-system/src/main.rs
+++ crates/bins/daoyi-module-system/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    daoyi_common_support::app::run(
+        Some(env!("CARGO_PKG_NAME")),
+        daoyi_api_system::create_router(),
+    )
+    .await
 }
