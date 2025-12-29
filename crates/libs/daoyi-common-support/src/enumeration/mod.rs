9eedcdebda4152764e5252e52f6c0277a16fd3a8
Gemiman <913573586@qq.com>
2025-12-29 23:58:05 +0800

    refactor(enum): 简化 CommonStatusEnum 的序列化实现
    - 移除自定义反序列化实现
    - 添加 Deserialize 派生宏自动生成反序列化代码
    - 清理不必要的 serde 导入项
    - 保持枚举的序列化功能不变

--- crates/libs/daoyi-common-support/src/enumeration/mod.rs
+++ crates/libs/daoyi-common-support/src/enumeration/mod.rs
@@ -2,8 +2,7 @@ pub mod redis_keys;
 
 use daoyi_macros::DaoyiIntoActiveValue;
 use sea_orm::prelude::*;
-use serde::de::{self, Visitor};
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(
     Debug,
@@ -39,6 +38,7 @@ pub enum Gender {
     PartialEq,
     Eq,
     Serialize,
+    Deserialize,
     EnumIter,
     DeriveActiveEnum,
     DaoyiIntoActiveValue,
@@ -51,58 +51,6 @@ pub enum CommonStatusEnum {
     #[sea_orm(string_value = "1")]
     #[serde(rename = "1")]
     Disable,
-}
-
-impl<'de> Deserialize<'de> for CommonStatusEnum {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct CommonStatusVisitor;
-
-        impl<'de> Visitor<'de> for CommonStatusVisitor {
-            type Value = CommonStatusEnum;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("integer 0/1 or string '0'/'1'")
-            }
-
-            fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                match v {
-                    0 => Ok(CommonStatusEnum::Enable),
-                    1 => Ok(CommonStatusEnum::Disable),
-                    _ => Err(E::custom(format!("Invalid status value: {}", v))),
-                }
-            }
-
-            fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                match v {
-                    0 => Ok(CommonStatusEnum::Enable),
-                    1 => Ok(CommonStatusEnum::Disable),
-                    _ => Err(E::custom(format!("Invalid status value: {}", v))),
-                }
-            }
-
-            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                match v {
-                    "0" => Ok(CommonStatusEnum::Enable),
-                    "1" => Ok(CommonStatusEnum::Disable),
-                    _ => Err(E::custom(format!("Invalid status value: {}", v))),
-                }
-            }
-        }
-
-        deserializer.deserialize_any(CommonStatusVisitor)
-    }
 }
 #[derive(
     Debug,
